eeaa881a56f6aa646ed6996da2e572bf7a8baf08
zakura0 <simon.kazemi99@gmail.com>
2025-12-02 18:54:22 +0100

    d02p2

--- d02/d02p2.py
+++ d02/d02p2.py
@@ -1,4 +1,31 @@
 with open ("input.txt") as f:
     ranges = [tuple(map(int, r.split("-"))) for r in f.read().strip().split(",")]
 
+result = 0
 
+for (rs, re) in ranges:
+    for i in range(rs, re + 1):
+        stri = str(i)
+        vlen = len(stri)
+        ls = ""
+        found = False
+        for j in range(1, vlen):
+            if found:
+                break
+            if stri[:j] in stri[j:]:
+                ls = stri[:j]
+                x = 2
+                while True and not ls == "":
+                    if ls * x == stri:
+                        result += i
+                        found = True
+                        break
+                    if len(ls * x) > vlen:
+                        break
+                    x += 1
+            else:
+                break
+print(result)
+
+
+            
