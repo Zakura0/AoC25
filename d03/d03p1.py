22f71114d67b034b2ce1012f12600368f7e3af72
zakura0 <simon.kazemi99@gmail.com>
2025-12-03 19:09:56 +0100

    d03p1

--- d03/d03p1.py
+++ d03/d03p1.py
@@ -1,4 +1,22 @@
 with open ("input.txt") as f:
     banks = f.read().strip().splitlines()
 
-banks = []
+result = 0
+
+for b in banks:
+    largest = "0"
+    for j in b:
+        if b.index(j) == len(b) - 1:
+            break
+        if int(j) > int(largest):
+            largest = j
+    index = b.index(largest)
+    second_largest = "0"
+    for i in range(index + 1, len(b)):
+        if int(b[i]) > int(second_largest):
+            second_largest = b[i]
+
+    result += int(str(largest) + str(second_largest))
+
+print(result)
+
