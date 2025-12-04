684280aabbf5c85ae999bc7b96d1fa67c17a1d5c
zakura0 <simon.kazemi99@gmail.com>
2025-12-04 17:07:26 +0100

    d04p1

---
+++ d04/d04p1.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r") as f:
+    grid = f.read().strip().splitlines()
+grid = [[c for c in l] for l in grid]
+
+height = len(grid)
+width = len(grid[0])
+
+result = 0
+
+for r in range(height):
+    for c in range(width):
+        if grid[r][c] == "@":
+            count = 0
+            for d in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                if r + d[0] >= 0 and r + d[0] < height and c + d[1] >= 0 and c + d[1] < width:
+                    if grid[r + d[0]][c + d[1]] == "@":
+                        count += 1
+            if count < 4:
+                result += 1
+
+print(result)
