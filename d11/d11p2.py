9eaedf7059a204a600e61d4737e7f25ed68ceb1e
zakura0 <simon.kazemi99@gmail.com>
2025-12-13 01:02:07 +0100

    hmmmm

--- d11/d11p2.py
+++ d11/d11p2.py
@@ -5,21 +5,26 @@ devices = {}
 for connection in connections:
     node, neighbors = connection.split(": ")
     devices[node] = neighbors.split()
+    def find_all_paths(devices, start, path, fft, dac):
+        if start == "fft":
+            fft = True
+        if start == "dac":
+            dac = True        
+        if start == "out":
+            if fft and dac:
+                return [path + ["out"]]
+            else:
+                return []
+        
+        path = path + [start]
+        paths = []
+        for neighbor in devices[start]:
+            if neighbor not in path:
+                new_path = find_all_paths(devices, neighbor, path, fft, dac)
+                paths.extend(new_path)
+        return paths
 
-def find_all_paths(devices, start, path):
-    if start == "out":
-        if "fft" in path and "dac" in path:
-                return 1
-        return 0            
-    path = path + [start]
-    paths = 0
-    for neighbor in devices[start]:
-        if neighbor not in path:
-            new_path = find_all_paths(devices, neighbor, path)
-            paths += new_path
-    return paths
-
-all_paths = find_all_paths(devices, "svr", [])
-print(all_paths)
+all_paths = find_all_paths(devices, "svr", [], False, False)
+print(len(all_paths))
 
     
