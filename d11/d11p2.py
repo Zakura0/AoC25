8d4fe6e5d5f1ea3761403f053799fb3da17c71fd
zakura0 <simon.kazemi99@gmail.com>
2025-12-13 01:21:56 +0100

    d11p2 dynamic programming mal wieder

--- d11/d11p2.py
+++ d11/d11p2.py
@@ -1,30 +1,35 @@
-with open("samplep2.txt", "r") as f:
+with open("input.txt", "r") as f:
     connections = f.read().strip().splitlines()
 
 devices = {}
 for connection in connections:
     node, neighbors = connection.split(": ")
     devices[node] = neighbors.split()
-    def find_all_paths(devices, start, path, fft, dac):
-        if start == "fft":
-            fft = True
-        if start == "dac":
-            dac = True        
-        if start == "out":
-            if fft and dac:
-                return [path + ["out"]]
-            else:
-                return []
-        
-        path = path + [start]
-        paths = []
-        for neighbor in devices[start]:
-            if neighbor not in path:
-                new_path = find_all_paths(devices, neighbor, path, fft, dac)
-                paths.extend(new_path)
-        return paths
 
-all_paths = find_all_paths(devices, "svr", [], False, False)
-print(len(all_paths))
+cache = {}
+
+def count_paths(devices, start, fft, dac):
+    if start == "fft":
+        fft = True
+    if start == "dac":
+        dac = True        
+    if start == "out":
+        if fft and dac:
+            return 1
+        return 0
+    
+    key = (start, fft, dac)
+    if key in cache:
+        return cache[key]
+    
+    total = 0
+    for neighbor in devices[start]:
+        total += count_paths(devices, neighbor, fft, dac)
+    
+    cache[key] = total
+    return total
+
+result = count_paths(devices, "svr", False, False)
+print(result)
 
     
